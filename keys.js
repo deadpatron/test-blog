34b00a19ef49a4252507f298d9b6b6492da4c336
deadpatron <31518999+deadpatron@users.noreply.github.com>
2019-01-07 13:47:54 +0300

    well done

---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://alex:123456q@ds149914.mlab.com:49914/fullstack_blog'
+}
---
+++ models/Post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('posts', postchema);
+
